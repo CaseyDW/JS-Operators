afc18ac89eef877acddb1127f5dda62123cb0068
Casey Ward <hitchkra@hotmail.com>
2023-03-07 20:50:56 -0500

    completed JS operators assingment

---
+++ app.js
@@ -0,0 +1,44 @@
+console.log("Hello World!\n==========\n");
+console.log(
+  "Follow the steps in the README.md file to complete the exercises:\n==========\n"
+);
+
+// Exercise 1
+console.log("EXERCISE 1:\n==========\n");
+
+let a = 20;
+let b = 4;
+let sum = a + b;
+let minus = a - b;
+let multiply = a * b;
+let dividing = a / b;
+
+console.log(a);
+console.log(sum);
+// Exercise 2
+console.log("EXERCISE 2:\n==========\n");
+
+let num = 11;
+let str = "11";
+let str2 = "eleven";
+let isPresent = true;
+let firstName = "Frodo";
+let lastName = "Baggins";
+
+console.log(num + str);
+console.log(num + str2);
+
+console.log
+
+// Exercise 3
+console.log("EXERCISE 3:\n==========\n");
+
+let val = 5;
+let str3 = "5";
+let str4 = "five";
+let isPresent2 = false;
+
+console.log(5 == "5");
+console.log(5 === "5");
+console.log("eleven" == str2 && val >= str);
+
